06a15960df33a55d8a8ff8d11b530133759a6b6c
C0mplex <gaybullayevodiljon@gmail.com>
2022-12-03 15:22:16 +0500

    work

--- 2022/3/main.py
+++ 2022/3/main.py
@@ -1,6 +1,51 @@
-f = open(r"C:\Users\User\PycharmProjects\adventofcode\2022\3\text.txt", "r")
-sum = 0
-result = 0
+f = open(r"C:\Users\User\PycharmProjects\adventofcode\2022\3\text2.txt", "r")
+
+natija = 0
+
+
+# def aniqla(str1, str2):
+#     for a in str1:
+#         for b in str2:
+#             if a == b:
+#                 if a != 'Z':
+#                     if ord(a) < 90:
+#                         res = ord(a) - 38
+#                     else:
+#                         res = ord(a) - 96
+#                 else:
+#                     res = 52
+#                 print(a + " : " + str(res))
+#                 return res
+#
+#
+# for x in f:
+#     x = x.replace('\n', '')
+#     natija = natija + aniqla(x[0:int(len(x) / 2)], x[int(len(x) / 2):len(x)])
+def aniqla(str1, str2, str3):
+    for a in str1:
+        for b in str2:
+            for c in str3:
+                if a == b == c:
+                    if a != 'Z':
+                        if ord(a) < 90:
+                            res = ord(a) - 38
+                        else:
+                            res = ord(a) - 96
+                    else:
+                        res = 52
+                    print(a + " : " + str(res))
+                    return res
+
+
+i = 0
 for x in f:
-    print(x)
-    print(x[0:int((len(x)+1)/2)])
+    i = i+1
+    x = x.replace('\n', '')
+    if i == 1:
+        x1 = x
+    if i == 2:
+        x2 = x
+    if i == 3:
+        i = 0
+        natija = natija + aniqla(x1, x2, x)
+print(natija)
